23c782aa7f617110c06dfa64c2ec7d60c7712d47
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-29 11:46:28 +0530

    server js updated

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ app.use("/api/items", itemRouter);
 // Ending Code
 app.use(errorHandler);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'));
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     })
+// }
 
 app.listen(port, () => {
     console.log(`Server is listen on port ${port}`);
