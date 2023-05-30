369d5f60217dcb169d77d5bb7a5f93aef490e46e
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-30 09:04:33 +0530

    updated code

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ app.use("/api/items", itemRouter);
 // Ending Code
 app.use(errorHandler);
 
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     })
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     });
 // }
 
 app.listen(port, () => {
