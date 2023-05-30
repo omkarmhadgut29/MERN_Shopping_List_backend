0eed17c9c8ecfefe88e115ea86ede9402b8d78db
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-30 11:26:03 +0530

    updated code v2

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 app.use(
     cors({
-        origin: ["https://mernshoppinglistfrontend-production.up.railway.app/"],
+        origin: ["https://mernshoppinglistfrontend-production.up.railway.app"],
     })
 );
 
